e6f6164384cc430c7347413198f02f1713eb7ba0
Tobias Andresen <tiltobias@gmail.com>
2025-11-11 12:49:36 +0100

    p2 A_matrix

--- data_project2_relative_phase/variables.py
+++ data_project2_relative_phase/variables.py
@@ -1,13 +1,13 @@
 T1 = 172800
 T2 = 175020
 
-base_position_llh = [
+base_known_llh = [
     -32.003884648,
     115.894802001,
     23.983
 ]
 
-rover_approx_position_llh = [
+rover_approx_llh = [
     -31.9,
     115.75,
     50.0
--- project2_relative_phase.py
+++ project2_relative_phase.py
@@ -2,19 +2,57 @@ import pandas as pd
 import numpy as np
 from project1_absolute_code import geodetic_to_cartesian
 
-from data_project2_relative_phase.variables import T1, T2, base_position_llh, rover_approx_position_llh
+from data_project2_relative_phase.variables import T1, T2, base_known_llh, rover_approx_llh
 
-base  = { "T1": pd.read_csv("data_project2_relative_phase/base_T1.csv"),
-          "T2": pd.read_csv("data_project2_relative_phase/base_T2.csv")}
-rover = { "T1": pd.read_csv("data_project2_relative_phase/rover_T1.csv"),
-          "T2": pd.read_csv("data_project2_relative_phase/rover_T2.csv")}
 
-"""
-Step 1: Transform receiver to Cartesian coordinates
-"""
 
-base_position_xyz = geodetic_to_cartesian(base_position_llh)
-rover_approx_position_xyz = geodetic_to_cartesian(rover_approx_position_llh)
-print("Base position (XYZ):", base_position_xyz)
-print("Rover approximate position (XYZ):", rover_approx_position_xyz)
+# def rho(receiver, satellite, T):
+#     rho_rec_sat = np.linalg.norm(satellite[["X","Y","Z"]].values - receiver)
+#     return 
 
+def A_matrix(satellites_rover, approx_rover_xyz):
+    A = []
+    for T in (T1, T2):
+        sat_rover_i = satellites_rover[T].iloc[0]
+        rho_B_i = np.linalg.norm([sat_rover_i["X"],sat_rover_i["Y"],sat_rover_i["Z"]] - approx_rover_xyz[:3])
+        for n in range(1, len(satellites_rover[T])):
+            sat_rover = satellites_rover[T].iloc[n]
+
+            rho_B_j = np.linalg.norm([sat_rover["X"],sat_rover["Y"],sat_rover["Z"]] - approx_rover_xyz[:3])
+            row = np.zeros(3 + len(satellites_rover[T]) -1)
+            for x, X in enumerate(["X","Y","Z"]):
+                row[x] = ( -( (sat_rover[X] - approx_rover_xyz[x]) / rho_B_j ) + ( (sat_rover_i[X] - approx_rover_xyz[x]) / rho_B_i ) )
+            row[3 + n-1] = 1
+            A.append(row)
+    return np.array(A)
+
+
+
+def main():
+    satellites_base  = {T1: pd.read_csv("data_project2_relative_phase/base_T1.csv"),
+                        T2: pd.read_csv("data_project2_relative_phase/base_T2.csv")}
+    satellites_rover = {T1: pd.read_csv("data_project2_relative_phase/rover_T1.csv"),
+                        T2: pd.read_csv("data_project2_relative_phase/rover_T2.csv")}
+    
+    """
+    Step 1: Transform receiver to Cartesian coordinates
+    """
+
+    base_known_xyz = geodetic_to_cartesian(base_known_llh)
+    rover_approx_xyz = geodetic_to_cartesian(rover_approx_llh)
+    print("Base position (XYZ):", base_known_xyz)
+    print("Rover approximate position (XYZ):", rover_approx_xyz)
+    # TODO: rename to llh and xyz in project1
+
+
+    """
+    Step 2: Observation equation, design matrix and delta L vector. Estimate rover position with Double difference.
+    """
+    print(A_matrix(satellites_rover, rover_approx_xyz))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
